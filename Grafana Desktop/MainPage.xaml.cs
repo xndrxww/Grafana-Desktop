753e99ad2e67548c5b51f388bc3a3c35e0a05fb9
Андрей Кузнецов <blacksilver@ro.ru>
2022-02-10 14:16:17 +0300

    Проверка

--- Grafana Desktop/MainPage.xaml.cs
+++ Grafana Desktop/MainPage.xaml.cs
@@ -15,9 +15,6 @@ using System.Windows.Shapes;
 
 namespace Grafana_Desktop
 {
-    /// <summary>
-    /// Логика взаимодействия для MainPage.xaml
-    /// </summary>
     public partial class MainPage : Page
     {
         public MainPage()
